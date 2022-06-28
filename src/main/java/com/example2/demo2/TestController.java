3caa35ebde2a2cfa1ea5ccad278b00ddee292e8f
Patrick <patrick.treppmann@rwth-aachen.de>
2022-06-28 21:03:38 +0200

    y

---
+++ src/main/java/com/example2/demo2/TestController.java
@@ -0,0 +1,12 @@
+package com.example2.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String greeting() {
+        return "hello world";
+    }
+}
