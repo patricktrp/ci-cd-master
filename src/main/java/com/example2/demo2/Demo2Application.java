c6bcb1226028227455f67c0282f313807e453406
Patrick <57516425+patricktrp@users.noreply.github.com>
2022-06-28 20:27:10 +0200

    Update Demo2Application.java

--- src/main/java/com/example2/demo2/Demo2Application.java
+++ src/main/java/com/example2/demo2/Demo2Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2Application {
 	public static void main(String[] args) {
-		//test
 		SpringApplication.run(Demo2Application.class, args);
 	}
 }
