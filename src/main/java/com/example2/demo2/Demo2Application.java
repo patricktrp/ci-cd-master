ef7a6addd39decf606840eeca60482dc9cbb6c83
Patrick <patrick.treppmann@rwth-aachen.de>
2022-06-28 15:04:39 +0200

    y

--- src/main/java/com/example2/demo2/Demo2Application.java
+++ src/main/java/com/example2/demo2/Demo2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
